3c3fad307c9f1dd9ed33cd72e5ee837d3677d805
JulsSal99 <116346078+JulsSal99@users.noreply.github.com>
2023-10-01 18:57:03 +0200

    Update prova.py

--- prova.py
+++ prova.py
@@ -142,4 +142,5 @@ if __name__ == '__main__':
     # random?
     # quante domande e risposte? 
     # Pause irrealistiche? 
+    # possibilità aggiunta rumore di fondo? 
     concatenate(testfile1, testfile2)
